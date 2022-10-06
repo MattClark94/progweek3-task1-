7636b70d4616e4e39d3161c381882cb527a044c7
Matt.Clark <mc1556@student.aru.ac.uk>
2022-10-06 13:06:34 +0100

    adding days of the week in

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿int grade1, grade2, grade3, grade4, grade5, grade6;
+int average; 
+
+Console.WriteLine(" Enter Grade 1 "); 
+grade1=Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine(" Enter Grade 2 "); 
+grade2=Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine(" Enter Grade 3 "); 
+grade3=Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine(" Enter Grade 4 "); 
+grade4=Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine(" Enter Grade 5 "); 
+grade5=Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine(" Enter Grade 6 "); 
+grade6=Convert.ToInt32(Console.ReadLine()); 
+
+average=grade1+grade2+grade3+grade4+grade5+grade6; 
+
+switch (average)
+{
+    case >=70: 
+        Console.WriteLine(" 1st "); 
+        break; 
+
+    case >=60: 
+        Console.WriteLine(" 2.1 ");
+        break; 
+
+    case >=50: 
+        Console.WriteLine(" 2.2 ");
+        break; 
+
+    case >=40: 
+        Console.WriteLine(" 3rd ");
+        break; 
+
+    case ==40: 
+        Console.WriteLine(" Pass ");
+        break; 
+    
+    case <40: 
+        Console.WriteLine(" Fail ");
+        break; 
+
+}
+
+Console.ReadKey(); 
